4856a781304609f0636cb777410397f78eb557ac
David Corrales <jdcorrales27@gmail.com>
2022-04-29 20:26:56 -0500

    adding some taks to code

--- mad_libs.py
+++ mad_libs.py
@@ -1,1 +1,3 @@
 ##Tomorrow I'll work in a new proyect for my experience
+
+# I need some text to create the game, and learn about adjetive, sustantive, noun, pro-noun and verbs. Because I dont know how to doing
